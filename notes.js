bd91f230613f1cf17faff5997330e45463bbee7c
ashwath <ashwathps@users.noreply.github.com>
2013-12-12 17:53:44 +0530

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,42 @@
+
+/*
+constructor function - A method that constructs an object.
+Eg: Object(), Number(), String(), Function() - convention: Use Uppercase names.
+Object() - returns an object
+Function() - returns a instance of function which in-turn is used to create new objects using 'new'
+*/
+
+//Custom functions can be defined using
+function MyCustom(){
+this.property1 = "something";
+}
+//implicitly returns 'this'
+
+/*
+Invocation context : Who is calling MyCustom?
+*/
+MyCustom(); //-> 'property1' will go into global/parent scope (eg: console).
+same as above:
+var x = MyCustom(); //although like alias, x is undefined.
+
+var mc = new MyCustom(); //-> 'property1' will go into the caller scope 'mc'
+
+//alias
+var mycust = function MyCustom1(val){
+this.property1 = val;
+}
+//alias looking.
+//c(); returns 'this' but doesn't pollute parent scope.
+//made to return an object explicitly.
+//its like any METHOD of JAVA or C++.
+//this is NOT prototypal inheritance.
+var c = function MyCustom2(val){
+// or var c = function (val){
+return {property2: val}
+};
+
+//objects are like a dictionary
+mc.property1;
+mc['property1'];
+//new keys can be added using [] or dot(.)
+
