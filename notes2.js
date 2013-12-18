a8b55f1e40a3f1108a120bd833dd2c9150218208
ashwath <ashwathps@users.noreply.github.com>
2013-12-18 17:41:18 +0530

    Update notes2.js

--- notes2.js
+++ notes2.js
@@ -209,7 +209,7 @@ which is an empty object. Below, we define a function called myFunction,
 then we access the prototype property, which is simply an empty object.
 */
 
-var myFunction = function() {};
+var myFunction = function() {}; //default value of prototype is Object()
 console.log(myFunction.prototype); // logs object{}
 console.log(typeof myFunction.prototype); // logs 'object'
 /*
@@ -219,3 +219,184 @@ It is only once we intend to use our function as a user-defined constructor func
 prototype property is leveraged, but this does not change the fact that the Function() 
 constructor gives each instance a prototype property.
 */
+
+//The value of a prototype property can be set to any of the complex values (
+// i.e., objects) available in JavaScript. 
+//JavaScript will ignore any prototype property  set to a primitive value.
+
+
+/*
+
+When object is created from a constructor function using the new keyword 
+(or when an object wrapper is created for a primitive value), it adds a hidden link between 
+the object instance created and the prototype property of the constructor function used to create it. 
+This link is known inside the instance as __proto__ 
+[though it is only exposed/supported via code in Firefox 2+, Safari, Chrome, and Android]. 
+JavaScript wires this together in the background when a constructor function is invoked and 
+it’s this link that allows the prototype chain
+
+*/
+
+Array.prototype.foo = 'foo';
+var myArray = new Array();
+
+console.log(myArray.__proto__.foo); //not a ECMA standard
+
+console.log(myArray.constructor.prototype.foo); //alternative, more verbose.
+
+//Careful! Anything added to Object.prototype will show up in a for in loop.
+
+
+/*
+
+Replacing the prototype Property with a New Object Removes the Default Constructor Property
+
+It’s possible to replace the default value of a prototype property with a new value. 
+Doing so, however, will eliminate the default constructor property found in 
+the “pre-made” prototype object—unless you manually specify one.
+*/
+
+var Foo = function Foo(){};
+
+Foo.prototype = {}; // replace prototype property with an empty object
+
+var FooInstance = new Foo();
+
+console.log(FooInstance.constructor === Foo); /* logs false, we broke the reference */
+console.log(FooInstance.constructor); // logs Object(), not Foo()
+
+//reversing the damage
+
+Foo.prototype = {constructor:Foo};
+var FooInstance = new Foo();
+
+console.log(FooInstance.constructor === Foo); // logs true
+
+//both are same.
+Foo.prototype = {x:1};
+
+Foo.prototype.x = 1;
+
+// BUT BUT, look closely
+// This is not same
+// Replacing the prototype Property with a New Object Does Not Update Former Instances
+var Foo = function Foo(){};
+Foo.prototype.x = 1;
+var FooInstance = new Foo();
+console.log(FooInstance.x); // logs 1, as you think it would
+
+// now let's replace/override the prototype object with a new Object() object
+Foo.prototype = {x:2};
+
+console.log(FooInstance.x); /* logs 1, WHAT? Shouldn't it log 2, we just updated prototype */
+/* FooInstance still references the same state of the prototype object that
+was there when it was instantiated. *
+
+Do Not Change prototype once instances are created */
+
+// access properties directly or Leverage prototype as a fallback - breaking the proto chain from running.
+
+var Person = function(legs, arms) {
+
+   // shadow prototype value
+   if (legs !== undefined) {this.legs = legs;}
+   if (arms !== undefined) {this.arms = arms;}
+};
+
+Person.prototype.legs = 2;
+Person.prototype.arms = 2;
+Person.prototype.countLimbs = function() {return this.legs + this.arms;};
+
+var chuck = new Person(0, 0);
+
+//Vs
+
+var Person = function() {};
+
+// all Person instances inherit a legs, arms, and countLimbs properties
+Person.prototype.legs = 2;
+Person.prototype.arms = 2;
+Person.prototype.countLimbs = function() {return this.legs + this.arms;};
+
+var chuck = new Person();
+
+// INHERITANCE
+
+Person.prototype.foo = 'foo';
+
+var Chef = function(){this.goo = 'goo'};
+Chef.prototype = new Person();
+var cody = new Chef();
+
+console.log(cody.foo); // logs 'foo'
+console.log(cody.goo); // logs 'goo'
+console.log(cody.bar); // logs 'bar'
+
+//ARRAY
+
+/*
+The length property of an array object can be used to get or set the length of an array. 
+As shown above, setting the length higher than the actual number of values contained in the 
+array will add undefined values to the array. What you might not expect is that you can actually 
+remove values from an array by setting the length value to a number less than the number of values 
+contained in the array. 
+*/
+
+var myArray = ['blue', 'green', 'orange', 'red'];
+console.log(myArray.length); // logs 4
+myArray.length = 99;
+console.log(myArray.length); /* logs 99, remember we set the length, 
+                             not an index */
+myArray.length = 1; // removed all but one value, so index [1] is gone!
+console.log(myArray[1]); // logs undefined
+
+console.log(myArray); // logs '["blue"]'
+
+//Primitive/Literal Values Are Converted to Objects When Properties Are Accessed
+
+/*
+Note
+
+Don’t confuse null with undefined. undefined is used by JavaScript to tell you that 
+something is missing. null is provided so you can determine when a value is expected but 
+just not available yet.
+
+Note
+
+When verifying a null value, always use === because == does not distinguish between null and 
+undefined.
+
+
+It is considered good practice to allow JavaScript alone to use undefined. 
+You should never find yourself setting a value to undefined, as in foo = undefined. 
+Instead, null should be used if you are specifying that a property or variable value is not available.
+
+
+*/
+
+console.log(undefined in this);  //true
+
+
+/*
+Other notes
+
+Math Is Not a Constructor Function
+
+The Math object is unlike the other built-in objects that are instantiated. 
+Math is a one-off object created to house static properties and methods, ready to be used when 
+dealing with numbers. Just remember, there is no way to create an instance of Math, 
+as there is no constructor.
+
+
+Many of the Math properties are constants that cannot be mutated. 
+Since this is a departure from the mutable nature of JavaScript, 
+these properties are in all-caps (e.g., Math.PI;). Do not confuse these property constants for constructor functions due to the capitalization of their first letter. They are simply object properties that cannot be changed.
+Note
+
+User-defined constants are not possible in JavaScript 1.5, ECMAScript 3 Edition.
+
+*/
+
+var Math = {
+PI : function(){console.log('pi called');}
+}
