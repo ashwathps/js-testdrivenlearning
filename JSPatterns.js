4b0783a929d127865be36ea3a540a372a97907a6
ashwath <ashwathps@users.noreply.github.com>
2014-01-02 19:07:09 +0530

    Create JSPatterns.js

---
+++ JSPatterns.js
@@ -0,0 +1,25 @@
+
+
+/*
+1.   Function argument pattern
+
+    using the 'arguments' argument that is passed to all functions in JS
+*/
+
+
+/*
+
+2.  Chaining
+*/
+
+/*
+3.  Observable property pattern
+    store incoming callbacks in an array and execute them upon appropriate action.
+*/
+
+/*
+4.  Timer pattern
+
+    setInterval & setTimeout
+*/
+
